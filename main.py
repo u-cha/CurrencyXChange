946c4d2c6efdc7fd1a969036d104d0640fdcb388
u-cha <s.udachin@mail.ru>
2023-03-01 17:28:54 +0400

    initial commit, first trials.
    - starting a local server with basic handler (do_GET)
    - creating a local database
    - creating Currency table inside the db

---
+++ main.py
@@ -0,0 +1,30 @@
+
+from http.server import BaseHTTPRequestHandler, HTTPServer
+import sqlite3
+
+
+class Handler(BaseHTTPRequestHandler):
+    def do_GET(self):
+        self.send_response(200)
+        self.send_header('Content-type', 'text/html')
+        self.end_headers()
+        self.wfile.write(bytes('<html><body><h1>Hello, Mr.Peterson!</h1></body></html>', 'utf-8'))
+
+
+server = HTTPServer(('127.0.0.1', 8080), Handler)
+
+db_connection = sqlite3.connect('local.db')
+db_cursor = db_connection.cursor()
+db_cursor.execute('''DROP TABLE Currencies''')
+db_cursor.execute('''CREATE TABLE Currencies (
+ID INT UNSIGNED AUTO_INCREMENT PRIMARY_KEY NOT NULL,
+Code VARCHAR(5) NOT NULL,
+FullName VARCHAR(50) NOT NULL,
+Sign VARCHAR(5) NOT NULL)''')
+db_connection.commit()
+
+
+
+# if __name__ == '__main__':
+    # server.serve_forever()
+
